310670d0c8550f6a402a205a6540c9a8b7073a52
Vladica777 <vladoiustefanalexandru@gmail.com>
2026-03-04 17:50:17 +0200

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -7,4 +7,10 @@ int main() {
   std::cerr << std::unitbuf;
 
    std::cout << "$ ";
+
+  std::string command;
+  std::getline(std::cin,command);
+  std::cout<<command <<": command not found";
+
+
 }
